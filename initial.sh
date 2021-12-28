24c0e11a18dcf9c6a2cac894b733d65e2023d487
Nirmal Thewarathanthri <nthewara@gmail.com>
2021-12-28 19:33:59 +0800

    added Portainer templates lib and shellinabox

--- initial.sh
+++ initial.sh
@@ -4,3 +4,9 @@ sudo apt install git -y
 
 curl -sSL https://get.docker.com | sh 
 sudo usermod -aG docker $USER
+
+
+Portainer Templates -> https://raw.githubusercontent.com/SelfhostedPro/selfhosted_templates/master/Template/portainer-v2.json
+
+Shellinabox -> sudo apt install shellinabox -y
+
